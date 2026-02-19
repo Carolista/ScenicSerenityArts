6fa63d26d2186c24b10c4a870fc68f36870c748c
Carolista <cjones.renaissancewoman@gmail.com>
2026-02-18 23:08:30 -0500

    created icon-link component and added instances to footer

--- js/components/footer.js
+++ js/components/footer.js
@@ -3,6 +3,8 @@
  * Creates a sticky footer that stays at the bottom of the viewport
  */
 
+import { createIconLink } from './icon-link.js';
+
 /**
  * Create footer element
  * @returns {HTMLElement} The footer element
@@ -10,8 +12,42 @@
 export function createFooter() {
 	const footer = document.createElement('footer');
 
+	// Create social icons container
+	const socialIcons = document.createElement('div');
+	socialIcons.className = 'social-icons';
+
+	// Create social media icon links
+	const facebookLink = createIconLink({
+		iconClass: 'fa-brands fa-facebook-f',
+		url: 'https://www.facebook.com/ScenicSerenityArts/',
+		tooltip: 'Facebook',
+	});
+
+	const instagramLink = createIconLink({
+		iconClass: 'fa-brands fa-instagram',
+		url: 'https://www.instagram.com/scenicserenityarts/',
+		tooltip: 'Instagram',
+	});
+
+	const etsyLink = createIconLink({
+		iconClass: 'fa-brands fa-etsy',
+		url: 'https://www.scenicserenityarts.etsy.com',
+		tooltip: 'Etsy',
+	});
+
+	socialIcons.appendChild(facebookLink);
+	socialIcons.appendChild(instagramLink);
+	socialIcons.appendChild(etsyLink);
+
+	footer.appendChild(socialIcons);
+
+	// Create copyright text
+	const copyright = document.createElement('div');
+	copyright.className = 'copyright';
 	const currentYear = new Date().getFullYear();
-	footer.innerHTML = `&copy; ${currentYear} Scenic Serenity Arts &bull; Chattanooga, TN`;
+	copyright.innerHTML = `&copy; ${currentYear} Scenic Serenity Arts &bull; Chattanooga, TN`;
+
+	footer.appendChild(copyright);
 
 	return footer;
 }
